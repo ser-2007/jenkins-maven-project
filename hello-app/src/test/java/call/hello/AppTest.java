4fae11edae456db2805a9030dbcbbc79020543ce
Serkan <sersah95@gmail.com>
2021-09-23 23:11:20 +0300

    test

--- hello-app/src/test/java/call/hello/AppTest.java
+++ hello-app/src/test/java/call/hello/AppTest.java
@@ -34,7 +34,7 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World from Java Maven Project!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World from Java Maven Project!." + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World from Java Maven Project!\"");
         }
